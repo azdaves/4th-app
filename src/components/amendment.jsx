cdd20985d7cbf4938ace248c6c715ff7f7d35888
Dave Schilling <ds@Daves-MacBook-Pro.local>
2018-10-13 14:47:55 -0700

    files to deploy app

---
+++ src/components/amendment.jsx
@@ -0,0 +1,170 @@
+import React, { Component } from "react";
+
+function ActiveQuestions(props) {
+  return (
+    <div>
+      <header>
+        <h2>Fourth Amendment Protection</h2>
+        <ul>
+          {props.list.map(question => (
+            <li key={question.name}>
+              <span>{question.name}</span>
+              <button onClick={() => props.onRemoveQuestion(question.name)}>
+                Remove
+              </button>
+              <button onClick={() => props.onToggleQuestion(question.name)}>
+                Deactivate
+              </button>
+            </li>
+          ))}
+        </ul>
+      </header>
+    </div>
+  );
+}
+function InactiveQuestions(props) {
+  return (
+    <div>
+      <header>
+        <h2>Not Protected</h2>
+        <ul>
+          {props.list.map(question => (
+            <li key={question.name}>
+              <span>{question.name}</span>
+              <button onClick={() => props.onToggleQuestion(question.name)}>
+                Activate
+              </button>
+            </li>
+          ))}
+        </ul>
+      </header>
+    </div>
+  );
+}
+
+class Amendment extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      questions: [
+        {
+          name: "Clothing you are wearing",
+          active: true
+        },
+        {
+          name: "A camper trailer you are driving in",
+          active: false
+        },
+        {
+          name: "Your iPhone",
+          active: true
+        },
+        {
+          name: "Your medication",
+          active: true
+        }
+      ],
+      input: ""
+    };
+
+    // getTotalQuestions = () => this.state.questions.length;
+    console.log("--constructor--");
+
+    this.handleRemoveQuestion = this.handleRemoveQuestion.bind(this);
+    this.updateInput = this.updateInput.bind(this);
+    this.handleAddQuestion = this.handleAddQuestion.bind(this);
+    this.handleToggleQuestion = this.handleToggleQuestion.bind(this);
+  }
+
+  componentDidUpdate() {
+    console.log("--componentDidUpdate--");
+  }
+  componentWillUnmount() {
+    console.log("--componentWillUnmount--");
+  }
+
+  handleAddQuestion() {
+    this.setState(currentState => {
+      return {
+        questions: currentState.questions.concat([
+          {
+            name: this.state.input,
+            active: true
+          }
+        ]),
+        input: ""
+      };
+    });
+  }
+  handleRemoveQuestion(name) {
+    this.setState(currentState => {
+      return {
+        questions: currentState.questions.filter(
+          question => question.name !== name
+        )
+      };
+    });
+  }
+  handleToggleQuestion(name) {
+    this.setState(currentState => {
+      const question = currentState.questions.find(
+        question => question.name === name
+      );
+      return {
+        questions: currentState.questions
+          .filter(question => question.name !== name)
+          .concat([
+            {
+              name,
+              active: !question.active
+            }
+          ])
+      };
+    });
+  }
+  updateInput(e) {
+    const value = e.target.value;
+    this.setState({
+      input: value
+    });
+  }
+  render() {
+    console.log("--render--");
+    return (
+      <div className="container">
+        <input
+          type="text"
+          placeholder="Add item"
+          value={this.state.input}
+          onChange={this.updateInput}
+        />
+        <button onClick={this.handleAddQuestion}>Submit</button>
+        <div>
+          <button
+            onClick={() =>
+              this.setState({
+                questions: []
+              })
+            }
+          >
+            Clear All
+          </button>
+        </div>
+        <ActiveQuestions
+          list={this.state.questions.filter(
+            question => question.active === true
+          )}
+          onRemoveQuestion={this.handleRemoveQuestion}
+          onToggleQuestion={this.handleToggleQuestion}
+        />
+        <InactiveQuestions
+          list={this.state.questions.filter(
+            question => question.active === false
+          )}
+          onToggleQuestion={this.handleToggleQuestion}
+        />
+      </div>
+    );
+  }
+}
+export default Amendment;
